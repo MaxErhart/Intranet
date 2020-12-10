0577692f54cb888e96d999d38001bfefcad9eaad
Max Erhart <merhart@web.de>
2020-12-10 14:39:44 +0100

    update

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,27 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About</h1>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'About',
+  components: {
+  },
+  data() {
+    return {
+
+    }
+  },  
+  mounted() {
+    this.$store.commit('setCurrentRoute', this.$store.getters.getRoutes[2])
+  },  
+}
+</script>
+
+<style lang="scss" scoped>
+  .about {
+    text-align: center;
+  }
+</style>
